4752bb4d7c1d9c7ab79159d0d3bee8b7f5eb7cba
dyl25 <dylan-vst@hotmail.com>
2021-03-13 10:50:10 +0100

    update package

--- src/components/ExerciseForm.vue
+++ src/components/ExerciseForm.vue
@@ -1,123 +1,257 @@
 <template>
-  <form action="" @submit.prevent="handleSubmit">
-    <div class="field">
-      <label class="label" for="title">Titre</label>
-
-      <div class="control">
-        <input v-model="form.title" class="input" type="text" name="title" id="title" >
-      </div>
-    </div>
-
-    <div class="columns">
-      <div class="column">
+    <form action="" @submit.prevent="handleSubmit">
         <div class="field">
-          <div class="select">
-            <select v-model="form.difficulty" name="difficulties" id="difficulties">
-              <option value="">-</option>
-              <option
-                v-for="difficulty in difficulties"
-                :key="difficulty.id"
-                value=""
-              >
-                {{ difficulty.name }}
-              </option>
-            </select>
-          </div>
-        </div>
-      </div>
+            <label class="label" for="title">Titre</label>
 
-      <div class="column">
+            <div class="control">
+                <input
+                    v-model="formData.title"
+                    class="input"
+                    type="text"
+                    name="title"
+                    id="title"
+                />
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label for="difficulties" class="label">Difficulté</label>
+                    <div class="control">
+                        <div class="select">
+                            <select
+                                v-model="formData.difficulty"
+                                name="difficulties"
+                                id="difficulties"
+                            >
+                                <option value="">-</option>
+                                <option
+                                    v-for="difficulty in difficulties"
+                                    :key="difficulty.id"
+                                    :value="difficulty.id"
+                                >
+                                    {{ difficulty.name }}
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <p class="has-text-centered label">Tag de l'exercice</p>
+
+                <div class="columns">
+                    <div class="column">
+                        <div class="field has-addons">
+                            <div class="control">
+                                <input
+                                    v-model="searchTerm"
+                                    class="input is-small"
+                                    type="text"
+                                    placeholder="Chercher un tag"
+                                />
+                            </div>
+                            <div class="control">
+                                <a class="button is-info is-small"> Ajouter </a>
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <span
+                                v-for="tag in filteredTagList"
+                                :key="tag.id"
+                                class="tag is-primary ml-1"
+                                @click="addTag(tag.name)"
+                            >
+                                {{ tag.name }}
+                            </span>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <p class="mb-3">Tags sélectionnés</p>
+                        <div>
+                            <span
+                                v-for="(tag, index) in formData.tags"
+                                :key="index"
+                                class="tag is-info ml-1"
+                            >
+                                {{ tag }}
+                                <button
+                                    class="delete is-small"
+                                    @click="removeTag(tag)"
+                                ></button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="field">
-          <span v-for="tag in tags" :key="tag.id" class="tag is-primary ml-1">
-            {{ tag.name }}
-            <button class="delete is-small"></button>
-          </span>
+            <label class="label" for="explenation">Explication</label>
+
+            <div class="control">
+                <textarea
+                    v-model="formData.explenation"
+                    name="explenation"
+                    id="explenation"
+                    class="textarea"
+                ></textarea>
+            </div>
         </div>
-      </div>
-    </div>
 
-    <div class="field">
-      <label class="label" for="explenation">Explication</label>
+        <div class="columns">
+            <div class="column">
+                <div class="file has-name is-boxed">
+                    <label class="file-label">
+                        <input
+                            class="file-input"
+                            type="file"
+                            name="image"
+                            accept="image/jpg, image/jpeg, image/png"
+                            @change="
+                                beginingImageChange($event.target.files[0])
+                            "
+                        />
+                        <span class="file-cta">
+                            <span class="file-icon">
+                                <i class="fas fa-upload"></i>
+                            </span>
+                            <span class="file-label"> Image de début </span>
+                        </span>
+                        <span class="file-name">
+                            {{
+                                this.formData.beginingImage
+                                    ? this.formData.beginingImage.name
+                                    : ""
+                            }}
+                        </span>
+                    </label>
+                </div>
+                <img
+                    v-if="formData.beginingImage"
+                    alt=""
+                    :src="displayImageUrl(formData.beginingImage)"
+                />
+            </div>
 
-      <div class="control">
-        <textarea
-          v-model="form.explenation"
-          name="explenation"
-          id="explenation"
-          class="textarea"
-        ></textarea>
-      </div>
-    </div>
-
-    <div class="columns">
-      <div class="column">
-        <div class="field">
-          <div class="file has-name is-boxed">
-            <label class="file-label">
-              <input class="file-input" type="file" name="image" />
-              <span class="file-cta">
-                <span class="file-icon">
-                  <i class="fas fa-upload"></i>
-                </span>
-                <span class="file-label"> Choisir une image </span>
-              </span>
-              <span class="file-name">
-                Screen Shot 2017-07-29 at 15.54.25.png
-              </span>
-            </label>
-          </div>
+            <div class="column">
+                <div class="file has-name is-boxed">
+                    <label class="file-label">
+                        <input
+                            class="file-input"
+                            type="file"
+                            name="imageEnd"
+                            accept="image/jpg, image/jpeg, image/png"
+                            @change="endingImageChange($event.target.files[0])"
+                        />
+                        <span class="file-cta">
+                            <span class="file-icon">
+                                <i class="fas fa-upload"></i>
+                            </span>
+                            <span class="file-label"> Image de fin </span>
+                        </span>
+                        <span class="file-name">
+                            {{
+                                this.formData.endingImage
+                                    ? this.formData.endingImage.name
+                                    : ""
+                            }}
+                        </span>
+                    </label>
+                </div>
+                <img
+                    v-if="formData.endingImage"
+                    alt=""
+                    :src="displayImageUrl(formData.endingImage)"
+                />
+            </div>
         </div>
-      </div>
-      <div class="column">
-        <div class="field">
-          <div class="file has-name is-boxed">
-            <label class="file-label">
-              <input class="file-input" type="file" name="video" />
-              <span class="file-cta">
-                <span class="file-icon">
-                  <i class="fas fa-upload"></i>
-                </span>
-                <span class="file-label"> Choisir une vidéo </span>
-              </span>
-              <span class="file-name">
-                Screen Shot 2017-07-29 at 15.54.25.png
-              </span>
-            </label>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-    <button class="button is-large is-success">Ajouter</button>
 
-  </form>
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <div class="file has-name is-boxed">
+                        <label class="file-label">
+                            <input
+                                class="file-input"
+                                type="file"
+                                name="video"
+                                accept="video/mp4, video/webm, image/gif"
+                                @change="videoChange($event.target.files[0])"
+                            />
+                            <span class="file-cta">
+                                <span class="file-icon">
+                                    <i class="fas fa-upload"></i>
+                                </span>
+                                <span class="file-label">
+                                    Vidéo
+                                </span>
+                            </span>
+                            <span class="file-name">
+                                {{
+                                    this.formData.video
+                                        ? this.formData.video.name
+                                        : ""
+                                }}
+                            </span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <button class="button is-large is-success">Ajouter</button>
+    </form>
 </template>
 
 <script>
-// @ is an alias to /src
 import axios from "axios";
 
 export default {
-  props: ["handleSubmit", "form"],
-  components: {},
+    props: ["handleSubmit", "modelForm"],
+    components: {},
 
-  mounted() {
-    this.getTags();
-    this.getDifficulties();
-  },
+    mounted() {
+        this.getTags();
+        this.getDifficulties();
+    },
 
-  data() {
-    return {
-      tags: [
-        {id: 1,name: 'abdos'}, {id: 2,name: 'bras'}, {id: 3,name: 'jambes'}
-      ],
-      difficulties: [],
-    };
-  },
+    computed: {
+        formData: {
+            get: function () {
+                return this.modelForm;
+            },
+            set: function (formData) {
+                this.$emit("update:modelForm", formData);
+            },
+        },
+        filteredTagList() {
+            return this.tags.filter((tag) => {
+                return tag.name
+                    .toLowerCase()
+                    .includes(this.searchTerm.toLowerCase());
+            });
+        },
+    },
 
-  methods: {
-    async getTags() {
-      /*await axios
+    data() {
+        return {
+            tags: [
+                { id: 1, name: "abdos" },
+                { id: 2, name: "bras" },
+                { id: 3, name: "jambes" },
+            ],
+            difficulties: [],
+            searchTerm: "",
+        };
+    },
+
+    methods: {
+        async getTags() {
+            /*await axios
         .get("/api/tags", {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("user_token"),
@@ -127,20 +261,57 @@ export default {
         .catch((err) =>
           console.log("Les tags n'ont pas put être chargés:" + err)
         );*/
-    },
+        },
 
-    async getDifficulties() {
-      await axios
-        .get("/api/difficulties", {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("user_token"),
-          },
-        })
-        .then((resp) => (this.difficulties = resp.data))
-        .catch((err) =>
-          console.log("les difficultés n'ont pas put être chargés" + err)
-        );
+        async getDifficulties() {
+            await axios
+                .get("/api/difficulties", {
+                    headers: {
+                        Authorization:
+                            "Bearer " + localStorage.getItem("user_token"),
+                    },
+                })
+                .then((resp) => (this.difficulties = resp.data))
+                .catch((err) =>
+                    console.log(
+                        "les difficultés n'ont pas put être chargés" + err
+                    )
+                );
+        },
+
+        selectTag(tagId) {
+            if (!this.formData.tags.includes(tagId)) {
+                this.formData.tags.push(tagId);
+            }
+        },
+
+        addTag(tag) {
+            if (!this.formData.tags.includes(tag)) {
+                this.formData.tags.push(tag);
+            }
+        },
+
+        removeTag(tag) {
+            this.formData.tags = this.formData.tags.filter(
+                (formTag) => formTag !== tag
+            );
+        },
+
+        beginingImageChange(file) {
+            this.formData.beginingImage = file;
+        },
+
+        endingImageChange(file) {
+            this.formData.endingImage = file;
+        },
+
+        videoChange(file) {
+            this.formData.video = file;
+        },
+
+        displayImageUrl(file) {
+            return URL.createObjectURL(file);
+        },
     },
-  },
 };
 </script>
--- src/views/exercise/Create.vue
+++ src/views/exercise/Create.vue
@@ -1,48 +1,78 @@
 <template>
-  <section class="section">
-    <h2 class="title">Création d'un exercice</h2>
-    <exercise-form :form="form" :handleSubmit="handleSubmit" />
-  </section>
+    <section class="section">
+        <h2 class="title">Création d'un exercice</h2>
+        <!--<exercise-form :form="form" @update:form="form = $event" :handleSubmit="handleSubmit" />-->
+        <exercise-form v-model:modelForm="form" :handleSubmit="handleSubmit" />
+
+    </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import axios from 'axios';
-import ExerciseForm from '@/components/ExerciseForm.vue';
+import axios from "axios";
+import ExerciseForm from "@/components/ExerciseForm.vue";
 
 export default {
-  components: {
-    ExerciseForm
-  },
-
-  mounted() {
-  },
-
-  data() {
-    return {
-      form: {
-        title: '',
-        explenation: '',
-        difficulty: null,
-        tags: [],
-        image: null,
-        video: null
-      }
-    };
-  },
-
-  methods: {
-    async handleSubmit() {
-      const url = process.env.VUE_APP_API_URL;
-
-      await axios.get(url + "/api/exercises", {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem('user_token'),
-        },
-      })
-      .then(resp => this.exercises = resp.data)
-      .catch(err => console.log('exercise list:' + err));
+    components: {
+        ExerciseForm,
     },
-  },
+
+    mounted() {},
+
+    data() {
+        return {
+            form: {
+                title: "",
+                explenation: "",
+                difficulty: null,
+                tags: [],
+                beginingImage: null,
+                endingImage: null,
+                video: null,
+                userId: JSON.parse(localStorage.getItem('user')).id
+            },
+        };
+    },
+
+    methods: {
+        
+        createFormData(form) {
+            const formData = new FormData();
+
+            Object.keys(form).forEach(key => {
+                if(form[key]) {
+                    if(Array.isArray(form[key])) {
+                        form[key].forEach((item,i) => {
+                            formData.append(key+'['+ i +']', item)
+                        });
+                    }else{
+                        //if(!form[key]) {
+                            //formData.append(key, null);
+                    // }else{
+                        formData.append(key, form[key]);
+                        //}
+                    }
+                }
+            });
+
+            return formData;
+        },
+
+        async handleSubmit() {
+
+            const formData = this.createFormData(this.form);
+            console.log(formData);
+
+            await axios
+                .post("/api/exercises", formData,{
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("user_token")
+                        //'content-type': 'multipart/form-data'
+                    },
+                })
+                .then((resp) => (console.log(resp.data)))
+                .catch((err) => console.log("exercise list:" + err));
+        },
+    },
 };
 </script>
